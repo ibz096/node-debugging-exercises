f7b83c84176b2c4fa06f03d301668101a3744f28
Ibrahim Ahmed <ibrahim.ahmed1996@gmail.com>
2022-03-20 23:34:35 -0400

    Updated

--- exercise-02/index.js
+++ exercise-02/index.js
@@ -1,3 +1,13 @@
-module.exports = function add(a, b) {
-  return a + b;
+module.exports = function add(...args) {
+
+  if (args.some(arg => typeof arg === "string")) {
+    throw "Cannot call add with string"
+  }
+
+  let sum = 0;
+  for (let arg of args) {
+    sum += arg;
+  }
+
+  return sum;
 };
