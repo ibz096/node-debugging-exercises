a8e3e5354128e1e3e9b217d0f0096a72884bbc73
Ibrahim Ahmed <ibrahim.ahmed1996@gmail.com>
2022-03-25 19:22:11 -0400

    Updated

--- exercise-05/index.js
+++ exercise-05/index.js
@@ -14,25 +14,32 @@
  * Returns: [{id: 1, name: 'c'}, {id: 2, name: 'b'}]
  */
 module.exports = function mergeArrays(arr1, arr2, key) {
-  let result = [];
-
-  for (let i = 0; i < arr1.length; i++) {
-    result.push(arr1[i]);
+  // for (let i = 0; i < arr1.length; i++) {
+  //   result.push(arr1[i]);
+  // }
+  let mergedArray = [...arr1, ...arr2];
+  let store = {};
+  for ( let i = 0; i < mergedArray.length; i++ ) {
+    let id = mergedArray[i][key]
+    store[id] = mergedArray[i]
   }
 
-  for (let i = 0; i < arr2.length; i++) {
-    let found = false;
-    for (let j = 0; j < result.length; j++) {
-      if (result[j][key] === arr2[i][key]) {
-        result[j] = arr2[i];
-        found = true;
-        break;
-      }
-    }
-    if (!found) {
-      result.push(arr2[i]);
-    }
-  }
+  // console.log(store);
+  return (Object.values(store));
 
-  return result;
+  // for (let i = 0; i < arr2.length; i++) {
+  //   let found = false;
+  //   for (let j = 0; j < result.length; j++) {
+  //     if (result[j][key] === arr2[i][key]) {
+  //       result[j] = arr2[i];
+  //       found = true;
+  //       break;
+  //     }
+  //   }
+  //   if (!found) {
+  //     result.push(arr2[i]);
+  //   }
+  // }
+
+  // return result;
 };
