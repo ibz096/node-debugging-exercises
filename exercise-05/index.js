93adbf64aeb87e2839ad67252b927e00b45beb4c
Ibrahim Ahmed <ibrahim.ahmed1996@gmail.com>
2022-03-25 19:30:40 -0400

    Refactored

--- exercise-05/index.js
+++ exercise-05/index.js
@@ -14,32 +14,13 @@
  * Returns: [{id: 1, name: 'c'}, {id: 2, name: 'b'}]
  */
 module.exports = function mergeArrays(arr1, arr2, key) {
-  // for (let i = 0; i < arr1.length; i++) {
-  //   result.push(arr1[i]);
-  // }
   let mergedArray = [...arr1, ...arr2];
+  
   let store = {};
-  for ( let i = 0; i < mergedArray.length; i++ ) {
-    let id = mergedArray[i][key]
-    store[id] = mergedArray[i]
-  }
+  mergedArray.forEach((element) => {
+    let id = element[key];
+    store[id] = element;
+  })
 
-  // console.log(store);
-  return (Object.values(store));
-
-  // for (let i = 0; i < arr2.length; i++) {
-  //   let found = false;
-  //   for (let j = 0; j < result.length; j++) {
-  //     if (result[j][key] === arr2[i][key]) {
-  //       result[j] = arr2[i];
-  //       found = true;
-  //       break;
-  //     }
-  //   }
-  //   if (!found) {
-  //     result.push(arr2[i]);
-  //   }
-  // }
-
-  // return result;
+  return Object.values(store);
 };
